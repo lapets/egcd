4a05fab78bdb8bb20547992e83de4902ebb9f00d
Andrei Lapets <a@lapets.io>
2022-07-31 15:06:28 -0400

    Update linting rules and use human-readable exceptions; update package version.

--- src/egcd/egcd.py
+++ src/egcd/egcd.py
@@ -7,7 +7,7 @@ from typing import Tuple
 import doctest
 
 def egcd(b: int, n: int) -> Tuple[int, int, int]:
-    # pylint: disable=C0301 # Accommodates long link URLs.
+    # pylint: disable=line-too-long # Accommodate long link URLs on docstring.
     """
     Given two integers ``b`` and ``n``, returns ``(gcd(b, n), a, m)`` such that
     ``a*b + n*m == gcd(b, n)``.
