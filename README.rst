cb43444a163cd9e143a2b0c97eb52fa0909a9459
Andrei Lapets <a@lapets.io>
2024-03-10 00:23:38 -0500

    Add support for any number of arguments; handle errors; update doctests/README.

--- README.rst
+++ README.rst
@@ -2,7 +2,9 @@
 egcd
 ====
 
-Easy-to-import library with a basic, efficient, pure-Python implementation of the extended Euclidean algorithm.
+Pure-Python
+`extended Euclidean algorithm <https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm>`__
+implementation that supports any number of integer arguments.
 
 |pypi| |readthedocs| |actions| |coveralls|
 
@@ -42,7 +44,10 @@ Examples
 .. |egcd| replace:: ``egcd``
 .. _egcd: https://egcd.readthedocs.io/en/0.6.0/_source/egcd.html#egcd.egcd.egcd
 
-The function |egcd|_ is an efficient implementation of the `extended Euclidean algorithm <https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm>`__. It accepts two integer inputs ``b`` and ``n``, returning a tuple of the form ``(gcd(b, n), a, m)`` where the three integers in the tuple satisfy the `identity <https://en.wikipedia.org/wiki/B%C3%A9zout%27s_identity>`__ ``(a * b) + (n * m) == gcd(b, n)``:
+.. |math_gcd| replace:: ``math_gcd``
+.. _math_gcd: https://docs.python.org/3/library/math.html#math.gcd
+
+The function |egcd|_ is a pure-Python implementation of the `extended Euclidean algorithm <https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm>`__ that extends the functionality and interface of the built-in |math_gcd|_ function. When it is supplied two integer arguments ``a`` and ``b``, it returns a tuple of the form ``(g, s, t)`` where the three integers in the tuple satisfy the `identity <https://en.wikipedia.org/wiki/B%C3%A9zout%27s_identity>`__ ``(a * s) + (b * t) == g``:
 
 .. code-block:: python
 
@@ -54,6 +59,26 @@ The function |egcd|_ is an efficient implementation of the `extended Euclidean a
     (2, 2437250447493, -2431817869532)
     >>> egcd(pow(2, 50), pow(3, 50))
     (1, -260414429242905345185687, 408415383037561)
+
+However, any number of integer arguments can be supplied. When no arguments are supplied, the result is ``(0,)`` (just as ``math.gcd()`` evaluates to ``0``). In all other cases, the result contains the greatest common divisor of all the supplied integers and the coefficients of the generalized form of the `identity <https://en.wikipedia.org/wiki/B%C3%A9zout%27s_identity>`__:
+
+.. code-block:: python
+
+    >>> egcd(2, 4, 3, 9)
+    (1, -1, 0, 1, 0)
+    >>> 1 == ((-1) * 2) + (0 * 4) + (1 * 3) + (0 * 9)
+    1
+
+A succinct way to extract the greatest common divisor and the coefficients is to take advantage of Python's support for iterable unpacking via the `asterisk <https://docs.python.org/3/reference/expressions.html#expression-lists>`__ notation:
+
+.. code-block:: python
+
+    >>> bs = (26, 16, 34)
+    >>> (g, *cs) = egcd(*bs)
+    >>> (g, cs)
+    (2, [-3, 5, 0])
+    >>> g == sum(c * b for (c, b) in zip(cs, bs))
+    True
 
 Development
 -----------
--- src/egcd/egcd.py
+++ src/egcd/egcd.py
@@ -1,18 +1,22 @@
 """
-Easy-to-import library with a basic, efficient, pure-Python implementation
-of the extended Euclidean algorithm.
+Pure-Python
+`extended Euclidean algorithm <https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm>`__
+implementation that supports any number of integer arguments.
 """
 from __future__ import annotations
 from typing import Tuple
 import doctest
 
-def egcd(b: int, n: int) -> Tuple[int, int, int]:
+def egcd(*integers: int) -> Tuple[int, ...]:
     # pylint: disable=line-too-long # Accommodate long link URLs on docstring.
     """
-    Given two integers ``b`` and ``n``, returns ``(gcd(b, n), a, m)`` such that
-    ``a*b + n*m == gcd(b, n)``.
+    Given two integers ``a`` and ``b``, this function returns a tuple of the
+    form ``(g, s, t)`` such that ``g`` is the greatest common divisor of ``a``
+    and ``b`` and such that the identity ``g == (a * s)  + (b * t)`` is
+    satisfied.
 
-    This implementation is adapted from the sources below:
+    This implementation is adapted from the sources below, and extended to
+    support two or more integers:
 
     * `Extended Euclidean Algorithm <https://brilliant.org/wiki/extended-euclidean-algorithm/>`__
       on `Brilliant.org <https://brilliant.org>`__,
@@ -32,24 +36,98 @@ def egcd(b: int, n: int) -> Tuple[int, int, int]:
     >>> egcd(pow(2, 50), pow(3, 50))
     (1, -260414429242905345185687, 408415383037561)
 
+    To support more than two inputs (as the built-in :obj:`math.gcd` does),
+    this function can accept any number of inputs. In such cases, the greatest
+    common divisor of all of the inputs is returned, along with coefficients
+    that satisfy the generalized form of the identity.
+
+    >>> egcd(2, 2, 3)
+    (1, 0, -1, 1)
+    >>> egcd(13, 16, 17)
+    (1, 5, -4, 0)
+    >>> egcd(2, 4, 3, 9)
+    (1, -1, 0, 1, 0)
+
+    To follow the behavior of :obj:`math.gcd` in its base cases, this function
+    returns ``0`` if the input is empty and the sole entry if no arguments are
+    is supplied.
+
+    >>> egcd(5)
+    (5, 1)
+    >>> egcd()
+    (0,)
+
+    A succinct way to extract the greatest common divisor and the coefficients
+    is to take advantage of Python's support for iterable unpacking via the
+    `asterisk <https://docs.python.org/3/reference/expressions.html#expression-lists>`__
+    notation.
+
+    >>> bs = (26, 16, 34)
+    >>> (g, *cs) = egcd(*bs)
+    >>> (g, cs)
+    (2, [-3, 5, 0])
+    >>> g == sum(c * b for (c, b) in zip(cs, bs))
+    True
+
+    If an argument is not an integer, an exception is raised.
+
+    >>> egcd(1.2, 3, 4)
+    Traceback (most recent call last):
+      ...
+    TypeError: 'float' object cannot be interpreted as an integer
+
+    >>> egcd(1, 2.3)
+    Traceback (most recent call last):
+      ...
+    TypeError: 'float' object cannot be interpreted as an integer
+
     The example below tests the behavior of this function over a range of
     inputs using the built-in :obj:`math.gcd` function.
 
+
+    The example below tests the behavior of this function over a range of
+    input pairs using the built-in :obj:`math.gcd` function.
+
     >>> from math import gcd
+    >>> from itertools import product
     >>> checks = []
-    >>> for (b, n) in [(b, n) for b in range(200) for n in range(200)]:
-    ...    (g, a, m) = egcd(b, n)
-    ...    checks.append(g == a*b + n*m)
-    ...    checks.append(g == gcd(b, n))
-    >>> all(checks)
-    True
+    >>> for (a, b) in product(*([range(200)] * 2)):
+    ...    (g, s, t) = egcd(a, b)
+    ...    assert(g == gcd(a, b))
+    ...    assert(g == (a * s) + (b * t))
+
+    The more complex example below tests the behavior of this function over
+    a range of input sequences.
+
+    >>> checks = []
+    >>> for k in range(1, 5):
+    ...    for bs in product(*([range(100 // k)] * k)):
+    ...        (g, *cs) = egcd(*bs)
+    ...        assert(g == gcd(*bs))
+    ...        assert(g == sum(c * b for (c, b) in zip(cs, bs)))
     """
-    (x0, x1, y0, y1) = (1, 0, 0, 1)
-    while n != 0:
-        (q, b, n) = (b // n, n, b % n)
-        (x0, x1) = (x1, x0 - q * x1)
-        (y0, y1) = (y1, y0 - q * y1)
-    return (b, x0, y0)
+    if len(integers) == 0:
+        return (0,)
+
+    (g, cs) = (integers[0], (1,))
+    for (i, a) in enumerate(integers):
+        if not isinstance(a, int): # Check type of all arguments.
+            raise TypeError(
+                "'" + type(a).__name__ + "'" +
+                ' object cannot be interpreted as an integer'
+            )
+        if i == 0: # First argument is already assigned to ``g``.
+            continue
+
+        (s, t, x0, x1, y0, y1) = (g, a, 1, 0, 0, 1)
+        while t != 0:
+            (q, s, t) = (s // t, t, s % t)
+            (x0, x1) = (x1, x0 - q * x1)
+            (y0, y1) = (y1, y0 - q * y1)
+        (g, s, t) = (s, x0, y0)
+        cs = tuple(c * s for c in cs) + (t,)
+
+    return (g,) + cs
 
 if __name__ == '__main__':
     doctest.testmod() # pragma: no cover
